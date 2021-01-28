0b208318183f5ca7a3232f2146a50fd25968bb4e
Sunil Pandey <inkingmind@gmail.com>
2021-01-28 16:06:01 +0530

    hello probo pr

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,11 @@
 import { Probot } from "probot";
 
 export = (app: Probot) => {
-  app.on("issues.opened", async (context) => {
+  app.on("pull_request.opened", async (context) => {
+    app.log("PR trigerred")
     const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
+      body: "Welcome to Git PR, Glad you open it!",
     });
-    await context.octokit.issues.createComment(issueComment);
-  });
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+  await context.octokit.issues.createComment(issueComment);
+});
 };
